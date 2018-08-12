f0cfee3fc48abc81f376590276e30c31e434299d
sphall <simonhall37@hotmail.com>
2018-08-12 20:54:12 +0200

    Initial dev commit with gitignore and helloworld app file

---
+++ src/main/java/com/simon/apiconnect/ApiConnectApplication.java
@@ -0,0 +1,25 @@
+package com.simon.apiconnect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiConnectApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ApiConnectApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnectApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner run() throws Exception {
+		return args -> {
+			log.info("Hello World! ");
+		};
+	}
+}
