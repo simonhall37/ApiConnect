f2e5123110f11d5cda324ee76f37c1d9ceeaa987
sphall <simonhall37@hotmail.com>
2018-08-16 16:34:40 +0200

    Commented out some of the test functionality

--- src/main/java/com/simon/apiconnect/ApiConnectApplication.java
+++ src/main/java/com/simon/apiconnect/ApiConnectApplication.java
@@ -2,7 +2,6 @@ package com.simon.apiconnect;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,15 +10,17 @@ import org.springframework.context.annotation.Bean;
 import com.simon.apiconnect.domain.ApiConnection;
 import com.simon.apiconnect.domain.CredentialType;
 import com.simon.apiconnect.domain.Profile;
-import com.simon.apiconnect.services.ConnectionService;
 
 @SpringBootApplication
 public class ApiConnectApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(ApiConnectApplication.class);
 	
-	@Autowired
-	private ConnectionService conService;
+//	@Autowired
+//	private ConnectionService conService;
+//	
+//	@Autowired
+//	private ProfileRepository profileRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiConnectApplication.class, args);
@@ -30,15 +31,23 @@ public class ApiConnectApplication {
 		return args -> {
 			
 			Profile profile1 = new Profile(1,"profile1");
+			profile1.addConnection(new ApiConnection("zendesk","https://wasupport.zendesk.com/api/v2/",CredentialType.BASIC,System.getenv("ZEN_USER"),System.getenv("ZEN_TOKEN")));
+			profile1.addConnection(new ApiConnection("redmine", "https://issues.webanywhere.co.uk/", CredentialType.TOKEN, "X-Redmine-API-Key", System.getenv("RM_API_KEY")));
 			
-			profile1.addConnection(new ApiConnection(1,"zendesk","https://wasupport.zendesk.com/api/v2/",CredentialType.BASIC,System.getenv("ZEN_USER"),System.getenv("ZEN_TOKEN")));
-			profile1.addConnection(new ApiConnection(2,"redmine", "https://issues.webanywhere.co.uk/", CredentialType.TOKEN, "X-Redmine-API-Key", System.getenv("RM_API_KEY")));
+			Profile profile2 = new Profile(2,"profile2");
+			profile2.addConnection(new ApiConnection("zendesk","https://wasupport.zendesk.com/api/v2/",CredentialType.BASIC,System.getenv("ZEN_USER"),System.getenv("ZEN_TOKEN")));
+			profile2.addConnection(new ApiConnection("redmine", "https://issues.webanywhere.co.uk/", CredentialType.TOKEN, "X-Redmine-API-Key", System.getenv("RM_API_KEY")));
 			
-			final String out = conService.Connect(profile1.getByName("zendesk"), "tickets.json");
-			final String out2 = conService.Connect(profile1.getByName("redmine"), "users.json");
+//			profileRepo.save(profile1);
+//			profileRepo.save(profile2);
 			
-			log.info(out.substring(0, 200));
-			log.info(out2.substring(0, 200));
+//			final String out = conService.Connect(profile1.getByName("zendesk"), "tickets.json");
+//			final String out2 = conService.Connect(profile1.getByName("redmine"), "users.json");
+			
+//			log.info(out.substring(0, 200));
+//			log.info(out2.substring(0, 200));
+			
+			log.info("Started");
 			
 		};
 	}
