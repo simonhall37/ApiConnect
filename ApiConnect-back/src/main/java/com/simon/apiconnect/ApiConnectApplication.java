9e26f18c9bdec6aa99a26dc14ed515e14010ea15
shall <simonhall37@hotmail.com>
2018-08-30 17:02:58 +0200

    Load Organisations from csv on startup and persist

--- ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
@@ -1,16 +1,29 @@
 package com.simon.apiconnect;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.simon.apiconnect.domain.bundle.Organisation;
+import com.simon.apiconnect.services.CSVService;
+import com.simon.apiconnect.services.OrganisationRepository;
+
 @SpringBootApplication
 public class ApiConnectApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(ApiConnectApplication.class);
+	
+	@Autowired
+	private OrganisationRepository orgRepo;
+	
+	@Autowired
+	private CSVService csvService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiConnectApplication.class, args);
@@ -20,7 +33,13 @@ public class ApiConnectApplication {
 	public CommandLineRunner run() throws Exception {
 		return args -> {
 			
-			log.info("Started");
+			List<String[]> lines = csvService.readCSV("orgs.csv");
+			int num =0;
+			for (String[] l : lines) {
+				if (++num>1)orgRepo.save(new Organisation(Long.parseLong(l[0]),Long.parseLong(l[1]),l[2]));
+			}
+			
+			log.info("Started with " + orgRepo.count());
 			
 		};
 	}
