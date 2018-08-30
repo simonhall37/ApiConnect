31ae611b7390a0cf9cf21f9f016e9233b03f1beb
sphall <simonhall37@hotmail.com>
2018-08-31 00:10:10 +0200

    Read in metadata on startup

--- ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
@@ -1,6 +1,10 @@
 package com.simon.apiconnect;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.simon.apiconnect.domain.bundle.Organisation;
+import com.simon.apiconnect.domain.bundle.TimeCorrection;
 import com.simon.apiconnect.services.CSVService;
 import com.simon.apiconnect.services.OrganisationRepository;
 
@@ -33,10 +38,31 @@ public class ApiConnectApplication {
 	public CommandLineRunner run() throws Exception {
 		return args -> {
 			
-			List<String[]> lines = csvService.readCSV("orgs.csv");
+			Map<Long,Set<TimeCorrection>> corrections = new HashMap<>();
+			List<String[]> lines = csvService.readCSV("changes.csv");
 			int num =0;
 			for (String[] l : lines) {
-				if (++num>1)orgRepo.save(new Organisation(Long.parseLong(l[0]),Long.parseLong(l[1]),l[2]));
+				if (++num>1) {
+					TimeCorrection tc = new TimeCorrection(Long.parseLong(l[1]),Double.parseDouble(l[2]));
+					long orgId = Long.parseLong(l[0]);
+					if (corrections.containsKey(orgId))
+						corrections.get(orgId).add(tc);
+					else {
+						HashSet<TimeCorrection> temp = new HashSet<>();
+						temp.add(tc);
+						corrections.put(orgId, temp);
+					}
+				}
+			}
+			
+			lines = csvService.readCSV("orgs.csv");
+			num =0;
+			for (String[] l : lines) {
+				if (++num>1) {
+					Organisation o = new Organisation(Long.parseLong(l[0]),Long.parseLong(l[1]),l[2]);
+					o.setCorrections(corrections.get(o.getZendeskId()));
+					orgRepo.save(o);
+				}
 			}
 			
 			log.info("Started with " + orgRepo.count());
