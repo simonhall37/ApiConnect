43e4f57a8bef92c5ffbfe541c8c81eb5ec06ab45
shall <simonhall37@hotmail.com>
2018-08-30 17:02:44 +0200

    Added method to read csv from classpath

--- ApiConnect-back/src/main/java/com/simon/apiconnect/services/CSVService.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/services/CSVService.java
@@ -1,5 +1,10 @@
 package com.simon.apiconnect.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +12,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class CSVService {
 
+	public List<String[]> readCSV(String name){
+		List<String[]> out = new ArrayList<>();
+		BufferedReader br = null;
+		
+		InputStream in = this.getClass().getClassLoader()
+                .getResourceAsStream(name);
+		try {
+			br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				out.add(line.split(","));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return out;
+	}
+	
 	public String wrapContents(List<Object> input) {
 		StringBuilder sb = new StringBuilder();
 		for (Object o : input) {
