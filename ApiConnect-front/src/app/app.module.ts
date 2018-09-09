b703ff71587509be7dc7695da014eb2421a28a37
sphall <simonhall37@hotmail.com>
2018-09-09 21:37:11 +0200

    Added more material modules

--- ApiConnect-front/src/app/app.module.ts
+++ ApiConnect-front/src/app/app.module.ts
@@ -4,19 +4,44 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule } from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from "@angular/common/http";
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CacheComponent } from './cache/cache.component';
+
+const appRoutes: Routes = [
+  { path: 'profiles', component: ProfileComponent }
+  , { path: 'cache',      component: CacheComponent }
+  // , { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProfileComponent,
+    CacheComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatTableModule,
+    MatIconModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    RouterModule.forRoot(
+      appRoutes
+      // , { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
