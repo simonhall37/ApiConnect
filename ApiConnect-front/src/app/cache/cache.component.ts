b72d0a1439922f5a90800365cd61b1e5a97ee017
sphall <simonhall37@hotmail.com>
2018-09-09 21:39:44 +0200

    Empty cache component

---
+++ ApiConnect-front/src/app/cache/cache.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'af-cache',
+  templateUrl: './cache.component.html',
+  styleUrls: ['./cache.component.scss'],
+
+})
+export class CacheComponent  {
+
+}
