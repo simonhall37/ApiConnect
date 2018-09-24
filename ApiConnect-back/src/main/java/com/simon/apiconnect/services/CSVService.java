4216ad372046fc93a3f1651d019c9fb1468d8e5b
shall <simonhall37@hotmail.com>
2018-09-24 15:34:52 +0200

    dd export functionality

--- ApiConnect-back/src/main/java/com/simon/apiconnect/services/CSVService.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/services/CSVService.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CSVService {
 
+	private static final Logger log = LoggerFactory.getLogger(CSVService.class);
+	
 	public List<String[]> readCSV(String name){
 		List<String[]> out = new ArrayList<>();
 		BufferedReader br = null;
@@ -35,6 +41,51 @@ public class CSVService {
 		}
 		
 		return out;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public String toCSV(List<Object> input, @SuppressWarnings("rawtypes") Class T, boolean includeHeader, List<String> toInclude) {
+		StringBuilder sb = new StringBuilder();
+		
+		if (includeHeader) {
+			for (String h : toInclude) {
+				sb.append(h + ",");
+			}
+			try{
+				sb.replace(sb.length()-1, sb.length(), "\n");
+			} catch(StringIndexOutOfBoundsException e) {
+				log.error(sb.toString() + " --- " + sb.length(),e);
+			}
+		}
+		
+		List<Method> methods = new ArrayList<>();
+		
+		for (String field : toInclude) {
+			try {
+				methods.add(T.getMethod("get"+field, new Class[] {}));
+			} catch (SecurityException | NoSuchMethodException e) {
+				log.error("Can't find field " + field + " in object "+ T.getSimpleName().toString());
+				return null;
+			}
+		}
+		
+		for (Object o : input) {
+			for (Method m : methods) {
+				try {
+					sb.append(m.invoke(o, new Object[] {}) + ",");
+				} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+					log.error("Can't access method " + m.getName(),e);
+					sb.append(",");
+				}
+			}
+			try{
+				sb.replace(sb.length()-1, sb.length(), "\n");
+			} catch(StringIndexOutOfBoundsException e) {
+				log.error(sb.toString() + " --- " + sb.length(),e);
+			}		
+		}
+		
+		return sb.toString();
 	}
 	
 	public String wrapContents(List<Object> input) {
