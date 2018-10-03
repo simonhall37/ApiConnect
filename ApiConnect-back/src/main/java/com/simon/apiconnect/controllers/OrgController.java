ebe2fc120b5ec86d70f66c18b3210fc8897c8d5f
shall <simonhall37@hotmail.com>
2018-10-03 16:48:47 +0200

    Added bundle and ticket REST endpoints

--- ApiConnect-back/src/main/java/com/simon/apiconnect/controllers/OrgController.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/controllers/OrgController.java
@@ -1,6 +1,9 @@
 package com.simon.apiconnect.controllers;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +15,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.simon.apiconnect.domain.statObj.StatBundle;
 import com.simon.apiconnect.domain.statObj.StatOrg;
+import com.simon.apiconnect.domain.statObj.StatTicket;
+import com.simon.apiconnect.services.BundleService;
+import com.simon.apiconnect.services.CSVService;
 import com.simon.apiconnect.services.StatOrgRepository;
 
 @Controller
@@ -24,6 +33,10 @@ public class OrgController {
 	private static final Logger log = LoggerFactory.getLogger(OrgController.class);
 	@Autowired
 	private StatOrgRepository statOrgRepo;
+	@Autowired
+	private BundleService bundleService;
+	@Autowired
+	private CSVService csvService;
 	
 	public OrgController (StatOrgRepository statOrgRepo) {
 		this.statOrgRepo = statOrgRepo;
@@ -44,6 +57,40 @@ public class OrgController {
 	public ResponseEntity<StatOrg> getOrg(@PathVariable long id){
 		return this.statOrgRepo.findByZendeskId(id).map(org -> new ResponseEntity<>(org,HttpStatus.OK))
 				.orElseThrow(() -> new NullPointerException("Organisation with id " + id + " not found"));
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/bundle/report")
+	public void getBundleReport(HttpServletResponse response) {
+		
+		List<Object> bundles = this.bundleService.getAllBundles();
+		List<String> toInclude = this.bundleService.getDefaultBundleColumns();
+		
+		response.setContentType("text/plain; charset=utf-8");
+		try {
+			response.getWriter().print(this.csvService.toCSV(bundles, StatBundle.class, true, toInclude));
+		} catch (JsonProcessingException e) {
+			log.error("JsonParse exception",e);
+		} catch (IOException e) {
+			log.error("IOException",e);
+		}
+	}
+	
+	@GetMapping
+	@RequestMapping(value = "/ticket/report")
+	public void getTicketReport(HttpServletResponse response) {
+		
+		List<Object> tickets = this.bundleService.getAllTickets();
+		List<String> toInclude = this.bundleService.getDefaultTicketColumns();
+		
+		response.setContentType("text/plain; charset=utf-8");
+		try {
+			response.getWriter().print(this.csvService.toCSV(tickets, StatTicket.class, true, toInclude));
+		} catch (JsonProcessingException e) {
+			log.error("JsonParse exception",e);
+		} catch (IOException e) {
+			log.error("IOException",e);
+		}
 	}
 	
 }
