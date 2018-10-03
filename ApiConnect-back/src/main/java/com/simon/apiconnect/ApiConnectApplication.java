560a80bb8e22aad2d182a31a6e4629a6610881a5
shall <simonhall37@hotmail.com>
2018-10-03 16:28:59 +0200

    Commented out the original test code and called importservice to import flat files

--- ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
+++ ApiConnect-back/src/main/java/com/simon/apiconnect/ApiConnectApplication.java
@@ -17,6 +17,7 @@ import com.simon.apiconnect.domain.CredentialType;
 import com.simon.apiconnect.domain.Profile;
 import com.simon.apiconnect.domain.statObj.StatBundle;
 import com.simon.apiconnect.domain.statObj.StatOrg;
+import com.simon.apiconnect.domain.statObj.StatTicket;
 import com.simon.apiconnect.services.BundleService;
 import com.simon.apiconnect.services.CSVService;
 import com.simon.apiconnect.services.ImportService;
@@ -54,28 +55,51 @@ public class ApiConnectApplication {
 			// load a default profile with permission to access Zendesk + Redmine
 			setupProfile();
 			
-			// import organisations from csv
+			
 			importService.initialOrgImport("stat/StatOrgs.csv","stat/StatBundles.csv","stat/StatCorrections.csv","stat/StatExtra.csv",false);
 			
-			// get tickets - replace in controller later on	
-			StatOrg cust1 = this.statOrgRepo.findByZendeskId(8359913647L).get();
-			this.bundleService.populateOrgTickets(cust1,false);
+//			
+//			StatOrg cust1 = this.statOrgRepo.findByZendeskId(8359913647L).get();
+//			this.bundleService.populateOrgTickets(cust1,false);
+//			
+//			
+//			List<String> toInclude = new ArrayList<>();
+//			toInclude.add("BundleNum");
+//			toInclude.add("OrgZenId");
+//			toInclude.add("StartDate");
+//			toInclude.add("EndDate");
+//			toInclude.add("BundleSize");
+//			toInclude.add("Balance");
+//			toInclude.add("Active");
+//			
+//			String out = this.csvService.toCSV(
+//					cust1.getBundles().stream().sorted().collect(Collectors.toList())
+//					, StatBundle.class, true, toInclude);
+//			System.out.println(out);
 			
-			// write the report out
-			List<String> toInclude = new ArrayList<>();
-			toInclude.add("BundleNum");
-			toInclude.add("OrgZenId");
-			toInclude.add("StartDate");
-			toInclude.add("EndDate");
-			toInclude.add("BundleSize");
-			toInclude.add("Balance");
-			toInclude.add("Active");
 			
-			String out = this.csvService.toCSV(
-					cust1.getBundles().stream().sorted().collect(Collectors.toList())
-					, StatBundle.class, true, toInclude);
-			System.out.println(out);
-			
+//			StatOrg cust2 = this.statOrgRepo.findByZendeskId(34757063L).get();
+//			this.bundleService.populateOrgTickets(cust2,false);
+//			
+//			
+//			List<String> toInclude2 = new ArrayList<>();
+//			toInclude2.add("CreatedDateTime");
+//			toInclude2.add("RequesterName");
+//			toInclude2.add("Subject");
+//			toInclude2.add("Type");
+//			toInclude2.add("Status");
+//			toInclude2.add("TotalEffort");
+//			
+//			String out2 = this.csvService.toCSV(
+//					cust2.getBundles().stream().sorted().collect(Collectors.toList())
+//					, StatBundle.class, true, toInclude);
+//			System.out.println(out2);		
+//
+//			String out3 = this.csvService.toCSV(
+//					cust2.getBundles().stream().findFirst().get().getTickets().stream().sorted().collect(Collectors.toList())
+//					, StatTicket.class, true, toInclude2);
+//			System.out.println(out3);		
+//			
 			log.info("Started");
 		};
 	}
